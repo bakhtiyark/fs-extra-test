fd691c145551223c4c3dc66f98d961fc4232f4f0
bakhtiyark <61044116+bakhtiyark@users.noreply.github.com>
2023-05-07 16:37:30 +0500

    Strings and RegEx

---
+++ CS and DT/String&RegEx&DateTime.js
@@ -0,0 +1,23 @@
+﻿/*
+Given the string 'ahb acb aeb aeeb adcb axeb'. 
+Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
+Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
+Get the day, month and year of the current date and output it to the console separately */
+
+const testStr = "ahb acb aeb aeeb adcb axeb";
+const numStr = "2 + 3 223 2223";
+
+const regStr = /a.b/gi;
+//console.log(testStr.match(regStr))
+const regNumStr = /2\s\+\s3/
+console.log(numStr.match(regNumStr))
+
+const now = Date();
+
+const day = now.slice(8, 10);
+const month = now.slice(4, 7);
+const year = now.slice(11, 15);
+
+console.log(day);
+console.log(month);
+console.log(year);
--- CS and DT/Strong&RegEx&DateTime.js
+++
@@ -1,5 +0,0 @@
-﻿/*
-Given the string 'ahb acb aeb aeeb adcb axeb'. 
-Write a regular expression that matches the strings ahb, acb, aeb by pattern: letter 'a', any character, letter 'b'
-Given the string '2 + 3 223 2223'. Write a regex that finds line 2 + 3 without capturing the rest
-Get the day, month and year of the current date and output it to the console separately */
